7b3d2532581e63f2df992204a02d6a82075384ed
Jack Tzu-Chieh Huang <tcjack@meepshop.com>
2014-07-22 14:28:59 +0800

    add snow theme

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var path = require('path');
 var superagent = require('superagent');
 
 
-gulp.task('default', function (cb) {
+gulp.task('copy-js', function (cb) {
 
 	var url = 'http://quilljs.com/js/quill.js';
 
@@ -26,4 +26,30 @@ gulp.task('default', function (cb) {
 		cb();
 	});
 
+});
+
+gulp.task('copy-css', function (cb) {
+	var url = 'http://quilljs.com/css/quill.snow.css';
+	if(!fs.existsSync('quilljs')) {
+		fs.mkdirSync('quilljs');
+	}
+	var agent = superagent.get(url)
+				.end(function (err, res) {
+					if(err || res.status !== 200) {
+						console.log(err);
+					}
+				});
+	var writer = fs.createWriteStream(path.resolve('quilljs', 'quill.snow.js'));
+	agent.pipe(writer);
+	writer.on('error', function(err) {
+		console.log(err);
+	});
+	writer.on('end', function() {
+		cb();
+	});
+});
+
+
+gulp.task('default', ['copy-js', 'copy-css'], function () {
+	console.log('files updated...');
 });
